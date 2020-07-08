f44bdfcd45ace8e05f211f46ffcc79d9bc9ce86a
maruichi-yuuta <nakapachi874@gmail.com>
2020-07-08 06:51:16 +0000

    Lv.4 finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+  root 'static_pages#home'
+  
+  get  'static_pages/home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get  'static_pages/help'
+  
+  get  'static_pages/about'
 
-Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  root 'application#hello'
 end
