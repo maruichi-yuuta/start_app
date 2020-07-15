0fc6e5033c9ff9b68aa5a981f3b2527e9441c014
maruichi-yuuta <nakapachi874@gmail.com>
2020-07-15 07:14:51 +0000

    Lv.pro

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,22 +11,29 @@ class UsersController < ApplicationController
     @users = User.all
   end
   
-  # def show 
+  def show 
+     @user = User.find(params[:id])
      
-  # end
-  
-  def create
-    User.create(user_params)
-    redirect_to root_path
   end
   
+  def create
+    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
   
   
   
   # private
   
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
   
   # def set_user
